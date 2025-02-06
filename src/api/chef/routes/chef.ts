c26e30e7117c1766ee676478503c9f1a525f15b3
cgilsanchez <cgilsanchez.02@gmail.com>
2025-02-06 14:38:55 +0100

    cambios chef.ts

--- src/api/chef/routes/chef.ts
+++ src/api/chef/routes/chef.ts
@@ -4,41 +4,31 @@
             method: 'GET',
             path: '/chefs',
             handler: 'chef.find',
-            config: {
-            auth: false,
-            },
+            config: { auth: false },
         },
         {
             method: 'GET',
             path: '/chefs/:id',
             handler: 'chef.findOne',
-            config: {
-            auth: false,
-            },
+            config: { auth: false },
         },
         {
             method: 'POST',
             path: '/chefs',
             handler: 'chef.create',
-            config: {
-            auth: false,
-            },
+            config: { auth: false },
         },
         {
             method: 'PUT',
             path: '/chefs/:id',
             handler: 'chef.update',
-            config: {
-            auth: false,
-            },
+            config: { auth: false },
         },
         {
             method: 'DELETE',
             path: '/chefs/:id',
             handler: 'chef.delete',
-            config: {
-            auth: false,
-            },
+            config: { auth: false },
         },
         ],
     };
