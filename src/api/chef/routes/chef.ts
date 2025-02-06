d2244a81da690b69d8f20648c434934a1f67d885
cgilsanchez <cgilsanchez.02@gmail.com>
2025-02-06 14:27:35 +0100

    chef.ts

--- src/api/chef/routes/chef.ts
+++ src/api/chef/routes/chef.ts
@@ -1,7 +1,45 @@
-/**
- * chef router
- */
-
-import { factories } from '@strapi/strapi';
-
-export default factories.createCoreRouter('api::chef.chef');
+    export default {
+        routes: [
+        {
+            method: 'GET',
+            path: '/chefs',
+            handler: 'chef.find',
+            config: {
+            auth: false,
+            },
+        },
+        {
+            method: 'GET',
+            path: '/chefs/:id',
+            handler: 'chef.findOne',
+            config: {
+            auth: false,
+            },
+        },
+        {
+            method: 'POST',
+            path: '/chefs',
+            handler: 'chef.create',
+            config: {
+            auth: false,
+            },
+        },
+        {
+            method: 'PUT',
+            path: '/chefs/:id',
+            handler: 'chef.update',
+            config: {
+            auth: false,
+            },
+        },
+        {
+            method: 'DELETE',
+            path: '/chefs/:id',
+            handler: 'chef.delete',
+            config: {
+            auth: false,
+            },
+        },
+        ],
+    };
+    
