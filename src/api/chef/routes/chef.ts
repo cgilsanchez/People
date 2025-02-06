1a5946d24415f1ca8f47ce2e5bfe055717d3e48f
cgilsanchez <cgilsanchez.02@gmail.com>
2025-02-06 16:57:36 +0100

    otra vez cambiado para poder hacer put

--- src/api/chef/routes/chef.ts
+++ src/api/chef/routes/chef.ts
@@ -1,35 +1,13 @@
     export default {
         routes: [
         {
-            method: 'GET',
-            path: '/chefs',
-            handler: 'chef.find',
-            config: { auth: false },
-        },
-        {
-            method: 'GET',
-            path: '/chefs/:id',
-            handler: 'chef.findOne',
-            config: { auth: false },
-        },
-        {
-            method: 'POST',
-            path: '/chefs',
-            handler: 'chef.create',
-            config: { auth: false },
-        },
-        {
             method: 'PUT',
             path: '/chefs/:id',
             handler: 'chef.update',
-            config: { auth: false },
-        },
-        {
-            method: 'DELETE',
-            path: '/chefs/:id',
-            handler: 'chef.delete',
-            config: { auth: false },
-        },
+            config: {
+            auth: { scope: ['authenticated'] }, // Permitir solo usuarios autenticados
+            },
+        }
         ],
     };
     
